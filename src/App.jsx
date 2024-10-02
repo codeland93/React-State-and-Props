69b7b9fb306ffb4647198daf2ae18eea0f82fe93
codeland93 <erichfreeland@gmail.com>
2024-10-02 00:16:15 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hooks from './Hooks'; // Adjust the path based on your file structure
+import StateAndProps from './StateAndProps'; // Adjust the path based on your file structure
+
+const App = () => {
+  return (
+    <div>
+      <Hooks />
+      <StateAndProps />
+    </div>
+  );
+}
+
+export default App;
